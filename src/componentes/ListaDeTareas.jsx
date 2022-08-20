47570182e3c93f183fb7ec716e5288973655595e
Facu Molas <facundomolasymolas@gmail.com>
2022-08-20 13:42:16 +0200

    Le dimos funcionalidad a los botones de agregaer tareas y eliminar

--- src/componentes/ListaDeTareas.jsx
+++ src/componentes/ListaDeTareas.jsx
@@ -8,8 +8,19 @@ const ListaDeTareas = () => {
   const [tareas,setTareas] = useState([]);
 
   const agregarTarea = tarea => {
-    console.log('Tarea agregada');
     console.log(tarea);
+    if(tarea.texto.trim()){
+      tarea.texto= tarea.texto.trim();
+
+      const tareasActualizadas = [tarea, ...tareas];
+      setTareas(tareasActualizadas);
+    }
+
+  }
+
+  const eliminarTarea = id => {
+    const tareasActualizadas = tareas.filter(tarea => tarea.id !== id);
+    setTareas(tareasActualizadas);
   }
 
   return (
@@ -19,7 +30,11 @@ const ListaDeTareas = () => {
             {
               tareas.map((tarea) =>
                 <Tarea
+                  key={tarea.id}
+                  id={tarea.id}
                   texto={tarea.texto}
+                  completada={tarea.completada}
+                  eliminarTarea={eliminarTarea}
                 />
               )
 
